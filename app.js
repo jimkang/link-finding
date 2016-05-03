588cee73eae3e1659d97dc320f6289b06a13e6d4
Jim Kang <jimkang@gmail.com>
2016-05-02 22:59:29 -0400

    Support descriptions/alt attributes.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var director = require('director');
 
 var routes = {
-  '/thing/:imgurl': renderImage,
+  '/thing/:imgurl/desc/:desc': renderImage,
   // '/concept/:concept': getConcept
 };
 
@@ -11,8 +11,9 @@ var routes = {
   router.init();
 })()))))));
 
-function renderImage(imgurl) {
+function renderImage(imgurl, desc) {
   var thingImg = document.querySelector('#thing');
   var imgSrc = decodeURIComponent(imgurl);
   thingImg.src = imgSrc;
+  thingImg.alt = decodeURIComponent(desc);
 }
