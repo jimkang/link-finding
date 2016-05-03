059d7d1df49b7ed9f8f8ea03987388d2bd34428d
Jim Kang <jimkang@gmail.com>
2016-05-03 15:54:09 -0400

    Support trimming.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var director = require('director');
 
+var linkMarginLeft = 32;
+
 var routes = {
   '/thing/:imgurl/desc/:desc': renderImage,
+  '/thing/:imgurl/desc/:desc/trim/': renderImageWithTrim,
   // '/concept/:concept': getConcept
 };
 
@@ -11,9 +14,32 @@ var routes = {
   router.init();
 })()))))));
 
-function renderImage(imgurl, desc) {
+function renderImageWithTrim(imgurl, desc) {
+  renderImage(imgurl, desc, true);
+}
+
+function renderImage(imgurl, desc, shouldTrim) {
   var thingImg = document.querySelector('#thing');
   var imgSrc = decodeURIComponent(imgurl);
   thingImg.src = imgSrc;
   thingImg.alt = decodeURIComponent(desc);
+
+  if (shouldTrim) {
+    setTimeout(resizePage, 500);
+  }
+}
+
+function resizePage() {
+  var thingImg = document.querySelector('#thing');
+  var linkImg = document.querySelector('#link');
+  
+  var pageWidth = thingImg.clientWidth;
+  if (pageWidth < linkImg.clientWidth + linkMarginLeft) {
+    pageWidth = linkImg.clientWidth + linkMarginLeft;
+  }
+  
+  var pageHeight = thingImg.clientHeight + linkImg.clientHeight;
+
+  document.body.style.width = pageWidth;
+  document.body.style.height = pageHeight;
 }
